42644c7a798ea5335c99dbff23cf2e7a92ce787c
Raul Santos <101840746+RaulSAFE@users.noreply.github.com>
2022-06-07 07:13:41 -0300

    Tarefa 4

--- main.c
+++ main.c
@@ -40,10 +40,13 @@ void tarefa_5(void);
 void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
+void tarefa_print(void);
+void tarefa_suspend(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
  */
+#define TAM_PILHA_0         (TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_1			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_2			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_3			(TAM_MINIMO_PILHA + 24)
@@ -52,11 +55,14 @@ void tarefa_8(void);
 #define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_PRINT     (TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_SUSPEND   (TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
  * Declaracao das pilhas das tarefas
  */
+uint32_t PILHA_TAREFA_0[TAM_PILHA_0];
 uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
 uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
 uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
@@ -65,6 +71,8 @@ uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
 uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
+uint32_t PILHA_TAREFA_PRINT[TAM_PILHA_PRINT];
+uint32_t PILHA_TAREFA_SUSP[TAM_PILHA_SUSPEND];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -77,9 +85,13 @@ int main(void)
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
 	
-	CriaTarefa(tarefa_7, "Tarefa 7", PILHA_TAREFA_7, TAM_PILHA_7, 1);
-	
-	CriaTarefa(tarefa_8, "Tarefa 8", PILHA_TAREFA_8, TAM_PILHA_8, 2);
+	CriaTarefa(tarefa_print, "Tarefa Print", PILHA_TAREFA_PRINT, TAM_PILHA_PRINT, 1);
+	CriaTarefa(tarefa_4, "Tarefa 4", PILHA_TAREFA_4, TAM_PILHA_4, 2);
+	CriaTarefa(tarefa_3, "Tarefa 3", PILHA_TAREFA_3, TAM_PILHA_3, 3);
+	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 4);
+	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 5);
+	CriaTarefa(tarefa_0, "Tarefa 0", PILHA_TAREFA_0, TAM_PILHA_0, 6);
+	CriaTarefa(tarefa_suspend, "Tarefa Suspender", PILHA_TAREFA_SUSP, TAM_PILHA_SUSPEND, 7);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -96,60 +108,89 @@ int main(void)
 	}
 }
 
+volatile uint16_t a = 0;
+volatile uint16_t b = 0;
+volatile uint16_t c = 0;
+volatile uint16_t d = 0;
+volatile uint16_t e = 0;
+
 /* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
-void tarefa_1(void)
+
+void tarefa_0(void)
 {
-	volatile uint16_t a = 0;
-	for(;;)
+	for (;;)
 	{
 		a++;
-		TarefaEspera(1000);
-		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE); /* Liga LED. */
-		
-		//TarefaContinua(2);
-		
+		TarefaContinua(1);
+		TarefaSuspende(6);
+	}
+}
+
+void tarefa_1(void)
+{
+ 
+	for(;;)
+	{
+		b++;
+		TarefaContinua(6);
+		TarefaSuspende(5);
 	}
 	
 }
 
 void tarefa_2(void)
 {
-	volatile uint32_t b;
+
 	for(;;)
 	{
-		b++;
-		TarefaEspera(500);	
-		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE); 	/* Turn LED off. */
-		
-		
+		c++;
+		TarefaContinua(5);
+		TarefaSuspende(4);
 	}
 }
 
 /* Tarefas de exemplo que usam funcoes para suspender as tarefas por algum tempo (atraso/delay) */
 void tarefa_3(void)
 {
-	volatile uint16_t a = 0;
+	
 	for(;;)
-	{
-		a++;	
-			
-		/* Liga LED. */
-		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
-		TarefaEspera(1000); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
-		
-		/* Desliga LED. */
-		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
-		TarefaEspera(1000); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+	{	
+		d++;
+		TarefaContinua(4);
+		TarefaSuspende(3);	
 	}
 }
 
 void tarefa_4(void)
 {
-	volatile uint16_t b = 0;
 	for(;;)
 	{
-		b++;
-		TarefaEspera(5);	/* tarefa se coloca em espera por 5 marcas de tempo (ticks) */
+		e++;
+		TarefaContinua(3);
+		TarefaSuspende(2);
+	}
+}
+
+void tarefa_print(void)
+{
+	for (;;)
+	{
+		volatile uint16_t sum = a + b + c + d + e;
+		TarefaSuspende(6);
+		TarefaContinua(2);
+		TarefaEspera(30000);
+	}
+}
+
+void tarefa_suspend(void)
+{
+	for (;;)
+	{
+		TarefaSuspende(3);
+		TarefaSuspende(4);
+		TarefaSuspende(5);
+		TarefaSuspende(6);
+		TarefaSuspende(7);
 	}
 }
 
